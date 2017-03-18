f0dfdb26764a5f917f6f5a4764dea16969af90c3
yinchendong <462085920@qq.com>
2017-03-18 09:04:45 +0800

    简历V1.0

---
+++ pro/rlzy/js/index.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Administrator on 2016/12/16 0016.
+ */
+function message(){
+    var a=document.getElementById("1");
+    a.style.backgroundColor="red"
+    var p=document.getElementById("mainpic");
+    p.style.backgroundImage="url(images/index_17.jpg)"
+
+}
+function message4(){
+    var a=document.getElementById("1");
+    a.style.backgroundColor="white"
+
+}
+var a=setInterval(message,5000)
+
+function message2(){
+    var a=document.getElementById("2");;
+    a.style.backgroundColor="red";
+    var p=document.getElementById("mainpic");
+    p.style.backgroundImage="url(images/index_172.jpg)"
+}
+function message5(){
+    var a=document.getElementById("2");
+    a.style.backgroundColor="white"
+
+}
+
+
+
+function message3(){
+    var a=document.getElementById("3");
+    a.style.backgroundColor="red"
+    var p=document.getElementById("mainpic");
+    p.style.backgroundImage="url(images/index_17.jpg)"
+}
+
+function message6(){
+    var a=document.getElementById("3");
+    a.style.backgroundColor="white"
+
+}
+
